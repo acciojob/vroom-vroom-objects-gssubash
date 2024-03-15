aa21b67b6f4c8158f581d444ec8d746da1d0e9dd
Subash Ganesan <74418430+gssubash@users.noreply.github.com>
2024-03-15 13:37:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -12,10 +12,17 @@ function SportsCar(make, model, topSpeed) {
 	Car.call(this,make,model);
 	this.topSpeed = topSpeed;
 }
-SportsCar.prototype.getMakeModel = function () {
+
+SportsCar.prototype = Object.create(Car.prototype);
+SportsCar.prototype.constructor=SportsCar;
+console.log(SportsCar.prototype.constructor);
+
+SportsCar.prototype.getTopSpeed = function () {
 	return this.topSpeed;
 }
-
+const car = new SportsCar("Ferrari", "Testarossa", 200);
+console.log(car.getMakeModel()); // Output: Ferrari Testarossa
+console.log(car.getTopSpeed()); // Output: 200
 // Do not change the code below
 window.Car = Car;
 window.SportsCar = SportsCar;
